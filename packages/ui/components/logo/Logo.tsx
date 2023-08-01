10df0c30e9716e2728430434de3f9250a6bc9d00
ffdietz <felipefierrod@gmail.com>
2023-08-01 22:56:51 +0200

    logo and navbar

---
+++ packages/ui/components/logo/Logo.tsx
@@ -0,0 +1,14 @@
+import { Box, Text } from '@chakra-ui/react'
+
+export default function Logo() {
+  return (
+    <Box width="full">
+      <Text fontFamily="" fontSize={32} fontWeight={400} lineHeight={1}>
+        FelipeFierroDietz
+      </Text>
+      <Text fontFamily="" fontSize={18}>
+        developer
+      </Text>
+    </Box>
+  );
+}
