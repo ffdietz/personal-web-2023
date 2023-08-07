3d42a81fa75438f53272ad6f48b8fa0a3aed4cf1
ffdietz <felipefierrod@gmail.com>
2023-08-07 23:11:56 +0200

    navbar conditional active section

--- packages/ui/components/hero/hero.tsx
+++ packages/ui/components/hero/hero.tsx
@@ -1,8 +1,8 @@
-import { Center, Flex, Heading, Stack } from "@chakra-ui/react";
+import { Heading, Stack } from "@chakra-ui/react";
 
 export function Hero() {
   return (
-    <Stack w="100vw" h="100vh" border="1px">
+    <Stack w="100vw" h="100vh" border="1px" mt={32}>
       <Heading as="h1">Hero</Heading>
     </Stack>
   );
--- packages/ui/components/navbar/navbar.tsx
+++ packages/ui/components/navbar/navbar.tsx
@@ -1,30 +1,63 @@
-import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { Flex, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import Logo from "../logo/logo";
 
 import { navLinks } from "@constants";
 
 export function Navbar() {
+  const [active, setActive] = useState<string | null>(null);
 
-const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-  e.preventDefault()
-  const targetId = e.currentTarget.href.replace(/.*\#/, "");
-  const section = document.getElementById(targetId);
-  window.scrollTo({
-    top: section?.getBoundingClientRect().top,
-    behavior: "smooth",
-  });
-};
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    const targetId = e.currentTarget.href.replace(/.*\#/, "");
+    const section = document.getElementById(targetId);
+    window.scrollTo({
+      top: section?.getBoundingClientRect().top,
+      behavior: "smooth",
+    });
+  };
 
   return (
-    <Flex bg="bgColor" minWidth="100vw" minHeight={38} padding={8}>
-      <Logo />
+    <Flex
+      bg="bgColor"
+      justifyContent="space-between"
+      minWidth="100vw"
+      position="fixed"
+      top={0}
+      px={16}
+      py={8}
+    >
+      <Link
+        href="/"
+        onClick={() => {
+          setActive("");
+          window.scrollTo(0, 0);
+        }}
+      >
+        <Logo />
+      </Link>
       <Flex gap={8}>
-        {navLinks.map((link) => (
-          <Link key={link.title} href={`#${link.id}`} onClick={handleScroll}>
-            {link.title}
-          </Link>
-        ))}
+        {navLinks.map((link) => {
+          return (
+            <Link
+              key={link.title}
+              href={`#${link.id}`}
+              onClick={(event) => {
+                handleScroll(event);
+                setActive(link.id);
+              }}
+            >
+              <Text
+                color={active === link.id ? "secondaryColor" : "textColor"}
+                fontSize={20}
+                textShadow="1px 1px accentColor"
+              >
+                {link.title}
+              </Text>
+            </Link>
+          );
+        })}
       </Flex>
     </Flex>
   );
