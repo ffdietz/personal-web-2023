8ca23eb7fc94b751747a9bf658f8b6acc006478a
ffdietz <felipefierrod@gmail.com>
2023-09-22 01:20:46 +0200

    content file

--- packages/ui/components/about/About.tsx
+++ packages/ui/components/about/About.tsx
@@ -1,7 +1,21 @@
+<<<<<<< Updated upstream
 import React from 'react'
 
 export function About() {
   return (
     <div>About</div>
+=======
+import { Center, Heading, Stack, Text } from "@chakra-ui/react";
+import { biography } from "@constants";
+
+export function About() {
+  return (
+    <Stack id="about" w="100vw" h="100vh" border="1px" paddingTop={32}>
+      <Heading>About</Heading>
+      <Center>
+        <Text as="h1" w="lg">{biography}</Text>
+      </Center>
+    </Stack>
+>>>>>>> Stashed changes
   )
 }
