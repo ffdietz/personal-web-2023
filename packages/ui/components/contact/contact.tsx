73834802e14d4fc4cdf1593c63d3ac1e1c63ce0d
ffdietz <felipefierrod@gmail.com>
2023-09-29 21:03:38 +0200

    contact icons

--- packages/ui/components/contact/contact.tsx
+++ packages/ui/components/contact/contact.tsx
@@ -1,10 +1,25 @@
-import { Heading, Stack } from "@chakra-ui/react"
+import { Box, Flex, Heading, Icon, Image, Stack, Text } from "@chakra-ui/react"
+import { contact } from "@constants"
 
 
 export function Contact() {
   return (
-    <Stack id="contact" w="100vw" h="100vh" border="1px">
-        <Heading as="h1">Contact</Heading>
-    </Stack>
-  )
+    <Flex
+      id="contact"
+      w="100vw"
+      h="100vh"
+      justifyContent="center"
+      align="center"
+      gap={16}
+    >
+        {contact.map((item) => {
+          return (
+            <Box key={item.title}>
+              {/* <Text>{item.title}</Text> */}
+              <Icon as={item.icon} boxSize={64} />
+            </Box>
+          );
+        })}
+    </Flex>
+  );
 }
