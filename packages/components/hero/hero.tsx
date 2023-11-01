a74453c19bee4ad0d9e97d125e9c01c5c4980752
ffdietz <felipefierrod@gmail.com>
2023-11-01 01:02:57 +0100

    hero mock up animation

--- packages/components/hero/hero.tsx
+++ packages/components/hero/hero.tsx
@@ -1,16 +1,26 @@
-import { Center, Stack, Text } from "@chakra-ui/react";
+import { Stack, Text } from "@chakra-ui/react";
 import { Section } from "@components/section";
 import { heroText } from "@content";
+import { HeroBackground } from "./hero-background";
 
 export function Hero() {
   return (
     <Section id="#">
-      <Stack w="100vw" h="100vh">
-        <Center h="full">
+      <Stack w="100vw" h="100vh" position="relative">
+        <Stack w="full" h="full" zIndex={0} top={0}>
+          <HeroBackground />
+        </Stack>
+        <Stack
+          position="absolute"
+          bg="transparent"
+          zIndex={1}
+          top="50vh"
+          left="50vw"
+        >
           <Text w="sm" fontSize="4xl">
             {heroText}
           </Text>
-        </Center>
+        </Stack>
       </Stack>
     </Section>
   );
