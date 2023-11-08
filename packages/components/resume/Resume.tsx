8f224b673e42816b2289dc872928e0174ca5e87c
ffdietz <felipefierrod@gmail.com>
2023-11-08 01:49:49 +0100

    resumen icon

--- packages/components/resume/Resume.tsx
+++ packages/components/resume/Resume.tsx
@@ -6,9 +6,12 @@ export function Resume() {
     <Link href="/FelipeFierro-Developer2023.pdf" locale={false}>
       <Button
         size="lg"
-        colorScheme="teal"
+        w={8}
+        h={14}
+        colorScheme="cyan"
         variant="outline"
         borderRadius="full"
+        borderWidth={4}
       >
         CV
       </Button>
